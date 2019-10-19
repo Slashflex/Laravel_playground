70c7af92c10680076df098eef130070b31d15e87
slashflex <pro.davidsaoud@gmail.com>
2019-10-19 23:21:07 +0200

    CRUD system

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BlogPost;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\StorePost;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('posts.index', ['posts' => BlogPost::all()]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, $id)
+    {
+        return view('posts.show', ['post' => BlogPost::findOrFail($id)]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(StorePost $request)
+    {
+        $validatedDate = $request->validated();
+        $blogPost = BlogPost::create($validatedDate);
+        $request->session()->flash('status', 'Blog post was created!');
+
+        return redirect()->route('posts.show', ['post' => $blogPost->id]);
+    }
+
+    public function edit($id)
+    {
+        $post = BlogPost::findOrFail($id);
+        return view('posts.edit', ['post' => $post]);
+    }
+
+    public function update(StorePost $request, $id)
+    {
+        $post = BlogPost::findOrFail($id);
+        $validatedDate = $request->validated();
+
+        $post->fill($validatedDate);
+        $post->save();
+        $request->session()->flash('status', 'Blog post was updated!');
+
+        return redirect()->route('posts.show', ['post' => $post->id]);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $post = BlogPost::findOrFail($id);
+        $post->delete();
+
+        // BlogPost::destroy($id);
+
+        $request->session()->flash('status', 'Blog post was deleted!');
+
+        return redirect()->route('posts.index');
+    }
+}
