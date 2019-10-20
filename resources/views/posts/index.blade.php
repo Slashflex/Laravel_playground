c12f2fc6b9142d4dbcc17306921bfcdd68dc18c9
slashflex <pro.davidsaoud@gmail.com>
2019-10-20 13:37:09 +0200

    Occurs when a post is new and less than 5 mins ago

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,9 +3,16 @@
 @section('content')
     @forelse($posts as $post)
         <p>
-            <h3>
-                <a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
-            </h3>
+            <!-- For New posts -->
+            @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 5)
+                <h3>
+                    <a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a><strong> New!</strong>
+                </h3>    
+            @else 
+                <h3>
+                    <a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
+                </h3>
+            @endif
 
             <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-primary">Edit</a>
         
