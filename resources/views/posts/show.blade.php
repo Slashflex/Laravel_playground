b82a656055acb7028f0527398913275e74db41e5
slashflex <pro.davidsaoud@gmail.com>
2019-10-22 17:33:32 +0200

    comments condition

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,4 +5,19 @@
     <p>{{ $post->content }}</p>
 
     <p>Added {{ $post->created_at->diffForHumans() }}</p>
+
+    <h4>Comments</h4>
+
+    @forelse($post->comments as $comment)
+        <p>
+            {{ $comment->content }}
+        </p>
+
+        <p class="text-muted">
+            added {{ $comment->created_at->diffForHumans() }}
+        </p>
+    @empty
+        <p>No comment yet!</p>
+    @endforelse
+
 @endsection
