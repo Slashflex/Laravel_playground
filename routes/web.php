01db672b41ec3058de1ecdc5d4e2ef13bab854b6
slashflex <pro.davidsaoud@gmail.com>
2019-10-23 18:41:17 +0200

    adds constraint to views base on user's status

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', 'HomeController@home')->name('home');
+    // ->middleware('auth');
 Route::get('/contact', 'HomeController@contact')->name('contact');
 Route::resource('/posts', 'PostController');
+
+Auth::routes();
