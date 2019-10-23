e096a49564552aa4496eb1ccbcda80cb62465cf1
slashflex <pro.davidsaoud@gmail.com>
2019-10-23 18:36:33 +0200

    dump and die User Object

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     public function home() 
     {
+        // dd(Auth::check());
+        // dd(Auth::user());
+        // dd(Auth::id());
         return view('home');
     }
 
