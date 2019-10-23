5e6702983ca69d71452bb89680d81a52a8b579fa
slashflex <pro.davidsaoud@gmail.com>
2019-10-23 23:47:16 +0200

    Defining calls to seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,13 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::cla
-        DB::table('users')->insert([
-            'name' => 'John Doe',
-            'email' => 'john@laravel.test',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+        // DB::table('users')->insert([
+        //     'name' => 'John Doe',
+        //     'email' => 'john@laravel.test',
+        //     'email_verified_at' => now(),
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'remember_token' => Str::random(10),
+        // ]);
+
+        $this->call([
+            UsersTableSeeder::class, 
+            BlogPostsTableSeeder::class, 
+            CommentsTableSeeder::class
         ]);
     }
 }
