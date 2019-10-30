913899b4f79ae8e4f6d44a4966024c9a50abaab9
slashflex <pro.davidsaoud@gmail.com>
2019-10-30 14:07:53 +0100

    using cache tags

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Cache;
+
 // use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
@@ -12,18 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // DB::table('users')->insert([
-        //     'name' => 'John Doe',
-        //     'email' => 'john@laravel.test',
-        //     'email_verified_at' => now(),
-        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        //     'remember_token' => Str::random(10),
-        // ]);
-        
         if ($this->command->confirm('Do you want to refresh the database?')) {
             $this->command->call('migrate:refresh');
             $this->command->info('Database was refreshed');
         }
+
+        Cache::tags(['blog-post'])->flush();
 
         $this->call([
             UsersTableSeeder::class, 
