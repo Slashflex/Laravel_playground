36b8f1b7415c0e1ae94b253fe7a9510ec28b22a1
slashflex <pro.davidsaoud@gmail.com>
2019-11-21 16:02:27 +0100

    View Composer with @include

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,35 +1,42 @@
 @extends('layout')
 
 @section('content')
-    <h1>
-        {{ $post->title }}
-        @badge(['show' => now()->diffInMinutes($post->created_at) < 30])
-            New !
-        @endbadge
-    </h1>
-    <p>{{ $post->content }}</p>
-    
-    @updated(['date' => $post->created_at, 'name' => $post->user->name]) 
-    @endupdated
-
-    @updated(['date' => $post->updated_at]) 
-        Updated
-    @endupdated
-
-    @tags(['tags' => $post->tags])@endtags
-
-    <p>Currently read by {{ $counter}} people</p>
-    
-    <h4>Comments</h4>
-
-    @forelse($post->comments as $comment)
-        <p>
-            {{ $comment->content }}
-        </p>
-
-        @updated(['date' => $comment->created_at]) 
+<div class="row">
+    <div class="col-8">
+        <h1>
+            {{ $post->title }}
+            @badge(['show' => now()->diffInMinutes($post->created_at) < 30])
+                New !
+            @endbadge
+        </h1>
+        <p>{{ $post->content }}</p>
+        
+        @updated(['date' => $post->created_at, 'name' => $post->user->name]) 
         @endupdated
-    @empty
-        <p>No comment yet!</p>
-    @endforelse
+
+        @updated(['date' => $post->updated_at]) 
+            Updated
+        @endupdated
+
+        @tags(['tags' => $post->tags])@endtags
+
+        <p>Currently read by {{ $counter}} people</p>
+        
+        <h4>Comments</h4>
+
+        @forelse($post->comments as $comment)
+            <p>
+                {{ $comment->content }}
+            </p>
+
+            @updated(['date' => $comment->created_at]) 
+            @endupdated
+        @empty
+            <p>No comment yet!</p>
+        @endforelse
+    </div>
+    <div class="col-4">
+        @include('posts._activity')
+    </div>
+</div>
 @endsection
