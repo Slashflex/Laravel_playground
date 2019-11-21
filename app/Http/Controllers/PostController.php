d13da4cf71efb0b8fb6fa8e6a58438c72593c3a5
slashflex <pro.davidsaoud@gmail.com>
2019-11-21 15:52:22 +0100

    Blade View Composers

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,26 +30,11 @@ class PostController extends Controller
      */
     public function index()
     {
-        $mostCommented = Cache::tags(['blog-post'])->remember('blog-post-commented', now()->addSeconds(10), function() {
-            return BlogPost::mostCommented()->take(5)->get();
-        });
-
-        $mostActive = Cache::remember('users-most-active', now()->addSeconds(10), function() {
-            return User::withMostBlogPosts()->take(5)->get();
-        });
-
-        $mostActiveLastMonth = Cache::remember('users-most-active-last-month', now()->addSeconds(10), function() {
-            return User::withMostBlogPostsLastMonth()->take(5)->get();
-        });
-
         return view(
             'posts.index',
             [
                 'posts' => BlogPost::latest()->withCount('comments')
-                    ->with('user')->with('tags')->get(),
-                'mostCommented' => $mostCommented,
-                'mostActive' => $mostActive,
-                'mostActiveLastMonth' => $mostActiveLastMonth,
+                    ->with('user')->with('tags')->get()   
             ]
         );
     }
--- app/Http/Controllers/PostTagController.php
+++ app/Http/Controllers/PostTagController.php
@@ -10,14 +10,6 @@ class PostTagController extends Controller
     public function index($tag) {
         $tag = Tag::findOrFail($tag);
 
-        return view(
-            'posts.index', 
-            [
-                'posts' => $tag->blogPosts, 
-                'mostCommented' => [], 
-                'mostActive' => [], 
-                'mostActiveLastMonth' => []
-            ]
-        );
+        return view('posts.index', ['posts' => $tag->blogPosts]);
     }
 }
